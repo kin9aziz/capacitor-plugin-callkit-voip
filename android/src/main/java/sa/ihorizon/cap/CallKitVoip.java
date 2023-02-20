6cc21083fa1c33788ea263d15bdfdcb2a95cb93d
Abdulaziz Alqahtani <kin9aziz@Abdulazizs-MacBook-Pro.local>
2023-02-20 17:11:42 +0300

    Initial commit

---
+++ android/src/main/java/sa/ihorizon/cap/CallKitVoip.java
@@ -0,0 +1,11 @@
+package sa.ihorizon.cap;
+
+import android.util.Log;
+
+public class CallKitVoip {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
